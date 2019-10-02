bca2ae8b6f5d597797dfb2b977640c6d55837519
Michael Butler <michaelbutler2000@outlook.com>
2019-10-02 19:49:48 +0100

    Updated XML comments

--- src/ProductService/ProductShared/IProductRepository.cs
+++ src/ProductService/ProductShared/IProductRepository.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 
 namespace ProductShared
 {
+    /// <summary>
+    /// Contract interface for getting product data
+    /// </summary>
     public interface IProductRepository
     {
+        /// <summary>Gets this instance.</summary>
+        /// <returns>A list of <see cref="ProductEntity"/> </returns>
         IEnumerable<ProductEntity> Get();
     }
 }
--- src/ProductService/ProductShared/ProductEntity.cs
+++ src/ProductService/ProductShared/ProductEntity.cs
@@ -4,16 +4,34 @@ using System.Text;
 
 namespace ProductShared
 {
+    /// <summary>
+    /// Product Entity class used internally and not for public consumption via API
+    /// </summary>
     public class ProductEntity
     {
+        /// <summary>
+        /// Unique product identifer
+        /// </summary>
         public Guid Id { get; set; }
 
+        /// <summary>
+        /// Product Code
+        /// </summary>
         public string Code { get; set; }
 
+        /// <summary>
+        /// Product Name
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Description of the product
+        /// </summary>
         public string Description { get; set; }
 
+        /// <summary>
+        /// URI to the main product image
+        /// </summary>
         public string PrimaryImageUri { get; set; }
     }
 }
